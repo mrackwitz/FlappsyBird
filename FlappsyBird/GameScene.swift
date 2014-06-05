4bf4735f3fc518e25c79068dc90be85dd8dea9cd
Marius Rackwitz <git@mariusrackwitz.de>
2014-06-05 15:07:09 +0200

    Added skyline to the scene

--- FlappsyBird/GameScene.swift
+++ FlappsyBird/GameScene.swift
@@ -13,6 +13,8 @@ class GameScene: SKScene {
     let skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
     
     let groundTexture = SKTexture(imageNamed: "Land")
+    let skyTexture    = SKTexture(imageNamed: "Sky")
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         setupScene()
@@ -23,12 +25,20 @@ class GameScene: SKScene {
         self.backgroundColor = skyColor
         
         self.setupGround()
+        self.setupSkyline()
     }
     
     func setupGround() {
         let sprite = SKSpriteNode(texture: groundTexture)
         sprite.setScale(2.0)
         sprite.position = CGPointMake(sprite.size.width / 2.0, sprite.size.height / 2.0)
+        self.addChild(sprite)
+    }
+    
+    func setupSkyline() {
+        let sprite = SKSpriteNode(texture: skyTexture)
+        sprite.setScale(2.0)
+        sprite.position = CGPointMake(sprite.size.width / 2.0, sprite.size.height / 2.0 + groundTexture.size().height * 2)
         self.addChild(sprite)
     }
     
