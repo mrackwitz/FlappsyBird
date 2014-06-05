d6dda19ba4f25b37d3a9d14f6cecef1dc82ad7a0
Marius Rackwitz <git@mariusrackwitz.de>
2014-06-05 15:02:23 +0200

    Added a blue sky background color

--- FlappsyBird/GameScene.swift
+++ FlappsyBird/GameScene.swift
@@ -9,8 +9,17 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let skyColor = SKColor(red: 81.0/255.0, green: 192.0/255.0, blue: 201.0/255.0, alpha: 1.0)
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        setupScene()
+    }
+    
+    func setupScene() {
+        // Setup background color
+        self.backgroundColor = skyColor
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
